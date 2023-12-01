ebe6bf95494e8c56b913114b7265d4c871b6fa2f
Kiryl Hlebau <Kirylhlebau@gmail.com>
2023-12-01 18:45:30 +0100

    erweitert

--- WorldCounter/src/com/example/Main.java
+++ WorldCounter/src/com/example/Main.java
@@ -2,8 +2,13 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
+
         WordCount test = new WordCount();
         test.worldcounter();
+        
+       
+    
+        
     }
 }
 
