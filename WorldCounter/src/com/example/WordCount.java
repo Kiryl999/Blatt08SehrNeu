f80eb1b946f5523e0e975a68175157cd482dadc9
Kiryl999 <Kirylhlebau@gmail.com>
2023-12-03 13:56:33 +0100

    formatString

--- WorldCounter/src/com/example/WordCount.java
+++ WorldCounter/src/com/example/WordCount.java
@@ -2,10 +2,13 @@ package com.example;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.lang.Object;
+import java.nio.charset.StandardCharsets;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.*;
@@ -14,6 +17,8 @@ public class WordCount {
 
     private static Set<String> words = new HashSet<>(); // unmodifiable set! Vielleicht TreeSet um alph. order
     private static Integer wordCount;
+    TreeMap<String, Integer> map = new TreeMap<>(); // Erstmal alle Unique Wörter zählen und anschließend in die TreeMap packen
+   
 
     // public WordCount(Set<String> getWords, int getWordCount) {
 
@@ -32,29 +37,37 @@ public class WordCount {
     
     public void worldcounter() {
     // String line = null;
-    
-    // FileInputStream propFile = new FileInputStream( "Grundgesetzt.txt");
-    // Properties p = new Properties(System.getProperties());
-    // p.load(propFile);
-    // System.setProperties(p);
-    // System.getProperties().list(System.out);
-    
+        
     String userHome = System.getProperty("user.home"); // user/home, also einfach das Homeverzeicnis
+    if(userHome == null | userHome.isEmpty()) {
+        throw new IllegalArgumentException("Kein Gültiges Verzeichnis gefunden");
+    }
+
     String filePath = userHome + File.separator + "Grundgesetz.txt";
+    if(!filePath.endsWith(".txt")) {
+        throw new IllegalArgumentException("Pfad führt nicht zu einer .txt Datei!");
+    }
+
     File file = new File(filePath);
-    TreeMap<String, Integer> map = new TreeMap<>(); // Erstmal alle Unique Wörter zählen und anschließend in die TreeMap packen
-
-
+    String MaxLange = "";
+   
+    
     try {
-        if (!file.exists() | !file.canRead()) {
+        if (!file.exists() | !file.canRead() | !file.isFile()) {
             throw new FileNotFoundException("Datei nicht gefunden oder nicht lesbar: " + filePath);
         }
-        try (BufferedReader f =  new BufferedReader(new FileReader(filePath))) {
-            for(String line = f.readLine(); line != null; line = f.readLine()) { // Vielleicht line!= null nicht richtig weil es gibt ja Absätze
-                String [] stringLineWords = line.toLowerCase().split("[\\s\\p{Punct}\\p{Cntrl}]+"); // Teilt jedes Wort in der Zeile in einem Array
+         try (BufferedReader f = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8))) {
 
+            for(String line = f.readLine(); line != null; line = f.readLine()) { 
+                String [] stringLineWords = line.toLowerCase().split("[\\s\\p{Punct}\\p{Cntrl}]+"); // Teilt jedes Wort in der Zeile in einem Array
+                
                 for(String wort : stringLineWords) { // Geht jedes Wort in Zeile durch
-                    if(wort.length() > 2) {
+
+                    if(wort.length() > MaxLange.length()) { // Finde längstes Wort
+                        MaxLange = wort;
+                    }
+
+                    if(wort.length() > 1) {
                         int count = map.containsKey(wort) ? map.get(wort) : 0; 
                         map.put(wort, count + 1); // Fügt Wort in Map ein, wenn vorhanden + 1
                     }   
@@ -71,14 +84,16 @@ public class WordCount {
 
     Map<String, Integer> unmodifiableMap = Collections.unmodifiableMap(map);
 
-    for(Map.Entry<String, Integer> entry : unmodifiableMap.entrySet()) {
-        if(entry.getValue() != null) {
-            System.out.printf("%-20s %10d%n", entry.getKey(), entry.getValue());
-        } else {
-            System.out.printf("%-20s %10d%n", entry.getKey(), "null");
+
+
+
+        String formatString = "| %-" + MaxLange.length() + "s | %6s |%n";  // Größe des Feldes für das Wort, gemessen anhand des größten wortes
+        System.out.printf(formatString, "Wort", "Anzahl");
+        for (Map.Entry<String, Integer> entry : unmodifiableMap.entrySet()) {
+            System.out.printf(formatString, entry.getKey(), entry.getValue());
         }
         
-    }
+    
 
     return;
     }
